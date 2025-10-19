66aa1c9c5fddc100a8ba43c6e81fb7f4ae623ab6
Alexey Poltoradnev <poltoradnevone@gmail.com>
2025-10-19 12:45:43 +0400

    simple web server implementation

---
+++ src/constants.ts
@@ -0,0 +1,10 @@
+export const MIME_TYPES = {
+  ".html": "text/html",
+  ".css": "text/css",
+  ".js": "application/javascript",
+  ".json": "application/json",
+  ".png": "image/png",
+  ".jpg": "image/jpeg",
+  ".svg": "image/svg+xml",
+  ".ico": "image/x-icon",
+};
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 import { createServer } from "node:http";
+import { constants, readFile } from "node:fs";
+import { dirname, join, normalize, extname } from "node:path";
+import { fileURLToPath, parse } from "node:url";
 import {
   getProducts,
   getProduct,
@@ -6,8 +9,13 @@ import {
   updateProduct,
   deleteProduct,
 } from "./controllers/product-controller.js";
+import { MIME_TYPES } from "./constants.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const PORT = process.env.PORT ? +process.env.PORT : 5000;
+const PUBLIC_DIR = join(__dirname, "..", "public");
 
 const server = createServer((req, res) => {
   if (req.url === "/api/products" && req.method === "GET") {
@@ -36,13 +44,44 @@ const server = createServer((req, res) => {
   } else if (req.url === "/api/products" && req.method === "POST") {
     createProduct(req, res);
   } else {
-    res.statusCode = 404;
-    res.setHeader("Content-Type", "application/json");
-    res.end(
-      JSON.stringify({
-        message: "Route Not Found",
-      })
-    );
+    const parsedUrl = parse(req.url!);
+    const pathname = parsedUrl.pathname;
+    const resolvedPath = normalize(pathname!).replace(/^(\.\.[\/\\])+/, "");
+    let filePath = join(PUBLIC_DIR, resolvedPath);
+
+    if (filePath.endsWith("/") || extname(filePath) === "") {
+      filePath = join(filePath, "index.html");
+    }
+
+    const ext = extname(filePath).toLowerCase();
+    const mimeType =
+      MIME_TYPES[ext as keyof typeof MIME_TYPES] || "application/octet-stream";
+
+    readFile(filePath, (error, content) => {
+      if (error) {
+        if (error.code === "ENOENT") {
+          res.statusCode = 404;
+          res.setHeader("Content-Type", "application/json");
+          res.end(
+            JSON.stringify({
+              message: "Route Not Found",
+            })
+          );
+        } else {
+          res.statusCode = 500;
+          res.setHeader("Content-Type", "application/json");
+          res.end(
+            JSON.stringify({
+              message: "Internal Server Error",
+            })
+          );
+        }
+      } else {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", mimeType);
+        res.end(content, "utf-8");
+      }
+    });
   }
 });
 
